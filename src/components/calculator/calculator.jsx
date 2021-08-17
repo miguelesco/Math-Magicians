d609aacc6d87a317684870632fab15add333209c
miguelesco <miuelangel1999@gmail.com>
2021-08-17 12:54:34 -0400

    styles-set-up

---
+++ src/components/calculator/calculator.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+import React, { PureComponent } from 'react';
+import Container from './styles';
+
+class Calculator extends PureComponent {
+  render() {
+    return (
+      <Container>
+        <h1>hola</h1>
+      </Container>
+    );
+  }
+}
+
+export default Calculator;
