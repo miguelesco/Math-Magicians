228b406e0771cbabdbecff8ada4ff8e8a6ed24b0
miguelesco <miuelangel1999@gmail.com>
2021-08-24 16:19:15 -0400

    routes testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import react from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Calculator from './components/calculator/calculator';
+import Home from './components/home/Home';
 
 class App extends react.PureComponent {
   render() {
     return (
-      <react.Fragment>
+      <Router>
         <GlobalStyles />
-        <Calculator />
-      </react.Fragment>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>FDSJDBSDJSDSJ DSDSDSDSDSDSDS</h1>
+  </div>
+);
+
+export default Home;
