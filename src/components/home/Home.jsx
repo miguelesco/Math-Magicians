421bd6b182f4efe5c80136ce4b4240ef44d35999
miguelesco <miuelangel1999@gmail.com>
2021-08-24 17:40:21 -0400

    home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,39 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
+import HomeContainer, { Title, Description } from './styles';
 
 const Home = () => (
-  <div>
-    <h1>FDSJDBSDJSDSJ DSDSDSDSDSDSDS</h1>
-  </div>
+  <HomeContainer>
+    <Title>Welcome to our Page!</Title>
+    <Description>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus quibusdam eos eveniet repellendus, sequi reprehenderit
+      iusto officia sit veniam fugiat, fugit assumenda dolorem. Asperiores
+      quam odio reiciendis nobis et consectetur.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus quibusdam eos eveniet repellendus, sequi reprehenderit
+      iusto officia sit veniam fugiat, fugit assumenda dolorem. Asperiores
+      quam odio reiciendis nobis et consectetur.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus quibusdam eos eveniet repellendus, sequi reprehenderit
+      iusto officia sit veniam fugiat, fugit assumenda dolorem. Asperiores
+      quam odio reiciendis nobis et consectetur.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus quibusdam eos eveniet repellendus, sequi reprehenderit
+      iusto officia sit veniam fugiat, fugit assumenda dolorem. Asperiores
+      quam odio reiciendis nobis et consectetur.
+      <br />
+      <br />
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus quibusdam eos eveniet repellendus, sequi reprehenderit
+      iusto officia sit veniam fugiat, fugit assumenda dolorem. Asperiores
+      quam odio reiciendis nobis et consectetur.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus quibusdam eos eveniet repellendus, sequi reprehenderit
+      iusto officia sit veniam fugiat, fugit assumenda dolorem. Asperiores
+      quam odio reiciendis nobis et consectetur.
+    </Description>
+  </HomeContainer>
 );
 
 export default Home;
